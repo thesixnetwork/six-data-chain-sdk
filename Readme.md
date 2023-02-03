518ce8a389bab36983f143c73d842a6e356a98e1
kanthakran-six <kanthakarn@six.network>
2023-02-03 14:57:51 +0700

    update export dependency fee

--- Readme.md
+++ Readme.md
@@ -17,7 +17,7 @@ yarn add six-data-chain-sdk
 ```
 ------
 ## EXAMPLE 
-You can try example in this link [SIX DATACHAIN EXAMPLE](https://github.com/thesixnetwork/six-data-chain-sdk/tree/release-v3.1.0/examples)
+You can try example in this link [SIX DATACHAIN EXAMPLE](https://github.com/thesixnetwork/six-data-chain-sdk/tree/release-v3.1.1/examples)
 
 ------
 <!-- ## LIBRARY STRUCTURE
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+
 globalThis.fetch = fetch
 
 export * from "./client/SixDataChainConnector"
@@ -6,3 +7,4 @@ export * from "./helper/base64"
 export * as ITxNFTmngr from "./modules/sixnft/thesixnetwork.sixnft.nftmngr/module/types/nftmngr/tx"
 export * as ITxNFTadmin from "./modules/sixnft/thesixnetwork.sixnft.nftadmin/module/types/nftadmin/tx"
 export * as ITxNFToracle from "./modules/sixnft/thesixnetwork.sixnft.nftoracle/module/types/nftoracle/tx"
+export * as fee from "@cosmjs/stargate/build/fee"
